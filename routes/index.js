c897429d7642cbaf2a51616ec48143fd3ecbcec6
BorbelyBettina <borbelybettina01@gmail.com>
2025-11-24 17:05:23 +0100

    főoldal

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  const cities = [
+    { id: 1, name: 'Szeged', description: 'Dóm tér és Szegedi Dóm, Szegedi Vadaspark, Fekete ház, Tisza-part sétány', image: '/images/szeged.jpg' },
+    { id: 2, name: 'Budapest', description: 'Parlament, Halászbástya és Budai Vár, Szent István Bazilika, Termálfürdők', image: '/images/budapest.jpg' },
+    { id: 3, name: 'Győr', description: 'Belváros és Barokk negyed, Győri Bazilika, Rába Quelle Élményfürdő, Széchenyi tér', image: '/images/győr.jpg' }
+  ];
+
+ const discountedTrips = [
+    { id: 101, name: 'Pécsi városnézés', description: '3 napos városlátogatás', image: '/images/pecs.jpg', price: 49900 },
+    { id: 102, name: 'Balatoni hétvége', description: '2 éjszaka a tóparton', image: '/images/siofok.jpg', price: 29900 }
+  ];
+
+  const testimonials = [
+  { name: 'Kovács Péter', city: 'Budapest', text: 'Nagyon elégedett vagyok a TigerTravel szolgáltatásaival. Minden gördülékenyen ment!' },
+  { name: 'Szabó Anna', city: 'Szeged', text: 'Fantasztikus utazás! Minden részletre odafigyeltek, szuper élmény volt.' },
+  { name: 'Nagy Gábor', city: 'Győr', text: 'Kiváló ajánlatok és profi csapat. Mindenképp ajánlom mindenkinek!' }
+];
+
+  res.render('index', { cities, discountedTrips, testimonials  }); 
 });
 
 module.exports = router;
